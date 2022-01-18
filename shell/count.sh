0eff2d652788184c6e2ec48fbf40ff67b68c01d1
Liang Tao <amethyst.black@gmail.com>
2022-01-18 18:58:17 +0800

    REFACTOR: Change the statistics method.

--- shell/count.sh
+++ shell/count.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#/usr/env bash
 
-awk '{ c[$0] += 1; } END{ for (t in c) { printf "%s\t%d\n", t, c[t]; } }'
+awk '{cnt[$2]=$1;i+=1;s+=$1;od[i]=$2;} END{for (i in od) {printf "%d\t%s\t%d\t%.2f%%\n", i, od[i], cnt[od[i]], cnt[od[i]] / s * 100;}}'
